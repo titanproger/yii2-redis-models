f8180d7425d1f7fcc39181bb8ee22c8e53031252
ProninE <titan.the.proger@gmail.com>
2018-10-22 17:39:26 +0300

    bug fix with hash model update counter

--- TRedisModelHash.php
+++ TRedisModelHash.php
@@ -13,11 +13,16 @@ class TRedisModelHash extends  TRedisModelBase
 {
     public function updateCounters($counters) {
         $key_name = $this->getRedisKeyName();
-        foreach($counters as $field => $delta) {
-            static::getRedisDb()->cmdHashFieldIncrement($key_name, $field , $delta );
+        foreach($counters as $name => $value) {
+            static::getRedisDb()->cmdHashFieldIncrement($key_name, $name , $value );
+            //duplicates \yii\db\BaseActiveRecord::updateCounters from here
+            if (!$this->hasAttribute($name))
+                $this->$name = $value;
+            else
+                $this->$name += $value;
+            $this->setOldAttribute($name, $this->$name);
         }
     }
-
 
     protected function saveToKey($key_name) {
         $result =  static::getRedisDb()->cmdHashSet( $key_name , $this->getAttributes(), $this->expire_time );
